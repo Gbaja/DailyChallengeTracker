04b2100de5598239791881c00ec5c71a05a3661e
Fatimat <gbajaf@yahoo.co.uk>
2021-11-15 20:32:21 +0000

    home-screen Home screen started

--- App.js
+++ App.js
@@ -1,111 +1,85 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import SVGImg from './src/assets/toDo.svg';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
+const App: () => Node = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
+    <View style={styles.container}>
+      <View style={styles.svgWrapper}>
+        <SVGImg width={300} height={300} style={styles.title} />
+      </View>
+      <Text style={styles.title}>Daily challenge tracker</Text>
+      <Text style={styles.subtitle}>
+        Set yourself a challenge to do an activity once a day. Track and log
+        your progress using this app.
       </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+      <TouchableOpacity
+        style={styles.loginButton}
+        onPress={() => console.log('Log in')}>
+        <Text style={styles.loginButtonText}>Login</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.signupButton}
+        onPress={() => console.log('Sign up')}>
+        <Text style={styles.signupButtonText}>Create Account</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    marginHorizontal: 15,
+    justifyContent: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  svgWrapper: {
+    // borderColor: 'red',
+    // borderWidth: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      height: 25,
+    },
+    shadowOpacity: 0.3,
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  title: {
+    fontWeight: 'bold',
+    fontSize: 30,
   },
-  highlight: {
-    fontWeight: '700',
+  subtitle: {
+    fontSize: 22,
+    marginTop: 30,
+  },
+  loginButton: {
+    alignItems: 'center',
+    backgroundColor: '#F50057',
+    padding: 10,
+    marginTop: 30,
+    shadowColor: '#000',
+    shadowOffset: {
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+  },
+  loginButtonText: {
+    color: 'white',
+    fontSize: 22,
+  },
+  signupButton: {
+    alignItems: 'center',
+    backgroundColor: 'white',
+    padding: 10,
+    marginTop: 30,
+    shadowColor: '#000',
+    shadowOffset: {
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+  },
+  signupButtonText: {
+    fontSize: 20,
   },
 });
 
